e7f328a17812379a7a14744cd3ea72434e1908d7
Timothy Bish <tabish121@gmail.com>
2021-06-04 17:29:16 -0400

    Add top level interfaces for type handlers

---
+++ src/Proton/Codec/ITypeDecoder.cs
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Apache.Qpid.Proton.Buffer;
+
+namespace Apache.Qpid.Proton.Codec
+{
+   /// <summary>
+   /// Defines an interface for an decoder of a specific type.
+   /// </summary>
+   /// <typeparam name="V">The type that this decoder manages</typeparam>
+   public interface ITypeDecoder<V>
+   {
+      /// <summary>
+      /// The Type that this decoder can read.
+      /// </summary>
+      /// <returns>The Type value that this type decoder decodes</returns>
+      Type DecodesType();
+
+      /// <summary>
+      /// Returns true if the value that this decoder is going to read is an array type.
+      /// </summary>
+      /// <returns>true if the decoder instance will read an array types</returns>
+      bool IsArrayType();
+
+      /// <summary>
+      /// Reads this decoders managed type from the given buffer and returns it.
+      /// </summary>
+      /// <param name="buffer">The buffer where the encoded bytes can be read from</param>
+      /// <param name="state">The decoder state that can be used during decode</param>
+      /// <returns></returns>
+      V ReadValue(IProtonBuffer buffer, IDecoderState state);
+
+      /// <summary>
+      /// Skiips the value that this decoder is handling by skipping the encoded bytes
+      /// in the provided buffer instance.
+      /// </summary>
+      /// <param name="buffer">The buffer where the encoded bytes can be read from</param>
+      /// <param name="state">The decoder state that can be used during decode</param>
+      void SkipValue(IProtonBuffer buffer, IDecoderState state);
+
+      /// <summary>
+      /// Reads a series of this type that have been encoded into the body of an Array type.
+      /// When encoded into an array the values are encoded in series following the identifier
+      /// for the type, this method is given a count of the number of instances that are encoded
+      /// and should read each in succession and returning them in a new array.
+      /// </summary>
+      /// <param name="buffer">The buffer where the encoded bytes can be read from</param>
+      /// <param name="state">The decoder state that can be used during decode</param>
+      /// <param name="count">the number of elements that the encoded array contains</param>
+      /// <returns></returns>
+      V[] ReadArrayElements(IProtonBuffer buffer, IDecoderState state, int count);
+
+   }
+}
---
+++ src/Proton/Codec/ITypeEncoder.cs
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Apache.Qpid.Proton.Buffer;
+
+namespace Apache.Qpid.Proton.Codec
+{
+   /// <summary>
+   /// Defines an interface for an encoder of a specific type.
+   /// </summary>
+   /// <typeparam name="V">The type that this encoder manages</typeparam>
+   public interface ITypeEncoder<V>
+   {
+      /// <summary>
+      /// The Type that this decoder can write.
+      /// </summary>
+      /// <returns>The Type value that this type encoder writes</returns>
+      Type DecodesType();
+
+      /// <summary>
+      /// Returns true if the value that this encoder is going to write is an array type.
+      /// </summary>
+      /// <returns>true if the decoder instance will read an array types</returns>
+      bool IsArrayType();
+
+      /// <summary>
+      /// Encodes the given value into the provided buffer. 
+      /// </summary>
+      /// <param name="buffer">The buffer where the encoded bytes are to be written</param>
+      /// <param name="state">The encoder state to use when writing the bytes</param>
+      /// <param name="value">The value to be encoded</param>
+      void WriteType(IProtonBuffer buffer, IEncoderState state, V value);
+
+      /// <summary>
+      /// Encodes a full array encoding of the given array elements into the provided buffer.
+      /// </summary>
+      /// <param name="buffer">The buffer where the encoded bytes are to be written</param>
+      /// <param name="state">The encoder state to use when writing the bytes</param>
+      /// <param name="value">The array value to be encoded</param>
+      void WriteArray(IProtonBuffer buffer, IEncoderState state, object[] value);
+
+      /// <summary>
+      /// Encodes only the individual elements of the given array into the provided buffer
+      /// </summary>
+      /// <param name="buffer">The buffer where the encoded bytes are to be written</param>
+      /// <param name="state">The encoder state to use when writing the bytes</param>
+      /// <param name="value">The array value to be encoded</param>
+      void WriteRawArray(IProtonBuffer buffer, IEncoderState state, object[] values);
+
+   }
+}
