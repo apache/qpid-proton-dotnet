894aea9069640489ee04493cabace5b781d7348e
Timothy Bish <tabish121@gmail.com>
2021-07-13 18:33:34 -0400

    Add more of the session API declaration

--- src/Proton/Engine/ISession.cs
+++ src/Proton/Engine/ISession.cs
@@ -16,7 +16,7 @@
  */
 
 using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using Apache.Qpid.Proton.Buffer;
 using Apache.Qpid.Proton.Engine.Exceptions;
 
@@ -32,6 +32,133 @@ namespace Apache.Qpid.Proton.Engine
       /// Provides access to the connection that owns this session endpoint.
       /// </summary>
       IConnection Connection { get; }
+
+      /// <summary>
+      /// Access the session state for the local end of the session.
+      /// </summary>
+      SessionState State { get; }
+
+      /// <summary>
+      /// Access the session state for the remote end of the session.
+      /// </summary>
+      SessionState RemoteState { get; }
+
+      /// <summary>
+      /// Returns an enumerator of all the receivers currently tracked by this session.
+      /// </summary>
+      IEnumerator<IReceiver> Receivers { get; }
+
+      /// <summary>
+      /// Returns an enumerator of all the senders currently tracked by this session.
+      /// </summary>
+      IEnumerator<ISender> Senders { get; }
+
+      /// <summary>
+      /// Create a new sender link using the provided name.
+      /// </summary>
+      /// <param name="name">The link name to assign the new sender</param>
+      /// <returns>The newly created sender instance</returns>
+      /// <exception cref="InvalidOperationException">If the session is closed"</exception>
+      ISender Sender(string name);
+
+      /// <summary>
+      /// Create a new receiver link using the provided name.
+      /// </summary>
+      /// <param name="name">The link name to assign the new sender</param>
+      /// <returns>The newly created receiver instance</returns>
+      /// <exception cref="InvalidOperationException">If the session is closed"</exception>
+      IReceiver Receiver(string name);
+
+      /// <summary>
+      /// Create a new transaction controller link using the provided name.
+      /// </summary>
+      /// <param name="name">The link name to assign the new sender</param>
+      /// <returns>The newly created transaction controller</returns>
+      /// <exception cref="InvalidOperationException">If the session is closed"</exception>
+      ITransactionController Coordinator(string name);
+
+      /// <summary>
+      /// Sets the maximum number of bytes this session can be sent from the remote.
+      /// </summary>
+      uint IncomingCapacity { get; set; }
+
+      /// <summary>
+      /// Gets the maximum number of bytes the remote session can be sent from the this session.
+      /// </summary>
+      uint RemoteIncomingCapacity { get; }
+
+      /// <summary>
+      /// Sets the maximum number of bytes this session can have written write before blocking
+      /// additional sends until the written bytes are known to have been flushed to the I/O.
+      /// This limit is intended to deal with issues of memory allocation when the I/O layer
+      /// allows for asynchronous writes and finer grained control over the pending write
+      /// buffers is needed.
+      /// </summary>
+      uint OutgoingCapacity { get; set; }
+
+      /// <summary>
+      /// Gets the maximum number of bytes the remote session can be have pending.
+      /// </summary>
+      uint RemoteOutgoingCapacity { get; }
+
+      /// <summary>
+      /// Set the handle max value for this Session which is the highest possible
+      /// link handle that can be open at one time before new attach request are
+      /// rejected.
+      /// <para/>
+      /// The handle max value can only be modified prior to a call to session open,
+      /// once the session has been opened locally an error will be thrown if this
+      /// method is called.
+      /// </summary>
+      uint HandleMax { get; set; }
+
+      /// <summary>
+      /// Gets the remote handle max to determine how many links could be attached
+      /// before the remote will refuse incoming attach requests.
+      /// </summary>
+      uint RemoteHandleMax { get; }
+
+      /// <summary>
+      /// Sets a delegate for when an AMQP Attach frame is received from the remote peer
+      /// for a sending link attach.
+      /// <para/>
+      /// Used to process remotely initiated sending link. Locally initiated links have their
+      /// own handlers invoked instead. This method is Typically used by servers to listen for
+      /// remote receiver creation. If an event handler for remote sender open is registered on
+      /// this Session for a link scoped to it then this handler will be invoked instead of the
+      /// variant in the Connection API.
+      /// </summary>
+      /// <param name="handler">The delegate that will handle this event</param>
+      /// <returns>This session instance</returns>
+      ISession ReceiverOpenHandler(Action<IReceiver> handler);
+
+      /// <summary>
+      /// Sets a delegate for when an AMQP Attach frame is received from the remote peer
+      /// for a receiving link attach.
+      /// <para/>
+      /// Used to process remotely initiated receiving link. Locally initiated links have their
+      /// own handlers invoked instead. This method is Typically used by servers to listen for
+      /// remote sender creation. If an event handler for remote sender open is registered on
+      /// this Session for a link scoped to it then this handler will be invoked instead of the
+      /// variant in the Connection API.
+      /// </summary>
+      /// <param name="handler">The delegate that will handle this event</param>
+      /// <returns>This session instance</returns>
+      ISession SenderOpenHandler(Action<ISender> handler);
+
+      /// <summary>
+      /// Sets a delegate for when an AMQP Attach frame is received from the remote peer
+      /// for a transaction manager link attach.
+      /// <para/>
+      /// Used to process remotely initiated transcation mangaers. Locally initiated links have
+      /// their own handlers invoked instead. This method is Typically used by servers to listen
+      /// for remote resource creation. If an event handler for remote sender open is registered on
+      /// this Session for a link scoped to it then this handler will be invoked instead of the
+      /// variant in the Connection API.
+      /// </summary>
+      /// <param name="handler">The delegate that will handle this event</param>
+      /// <returns>This session instance</returns>
+      ISession TransactionManagerOpenedHandler(Action<ITransactionManager> handler);
 
    }
 }
