4f1415e715718b73a4e99f6d4c76bc2c89abdc27
Timothy Bish <tabish121@gmail.com>
2022-03-16 17:12:57 -0400

    Add some new SslOptions values for future SSL work

--- src/Proton.Client/Client/SslOptions.cs
+++ src/Proton.Client/Client/SslOptions.cs
@@ -15,6 +15,9 @@
  * limitations under the License.
  */
 
+using System.Net.Security;
+using System.Security.Authentication;
+
 namespace Apache.Qpid.Proton.Client
 {
    /// <summary>
@@ -22,7 +25,7 @@ namespace Apache.Qpid.Proton.Client
    /// </summary>
    public class SslOptions
    {
-      public static readonly bool DEFAULT_TRUST_ALL = false;
+      public static readonly bool DEFAULT_ENABLED_CERT_REVOCATION_CHECKS = false;
       public static readonly bool DEFAULT_VERIFY_HOST = true;
       public static readonly int DEFAULT_SSL_PORT = 5671;
 
@@ -55,6 +58,13 @@ namespace Apache.Qpid.Proton.Client
       internal SslOptions CopyInto(SslOptions other)
       {
          other.DefaultSslPort = DefaultSslPort;
+         other.SslEnabled = SslEnabled;
+         other.VerifyHost = VerifyHost;
+         other.EnableCertificateRevocationChecks = EnableCertificateRevocationChecks;
+         other.CommonNameOverride = CommonNameOverride;
+         other.RemoteValidationCallbackOverride = RemoteValidationCallbackOverride;
+         other.LocalCertificateSelectionOverride = LocalCertificateSelectionOverride;
+         other.TlsVersionOverride = TlsVersionOverride;
 
          return this;
       }
@@ -68,6 +78,48 @@ namespace Apache.Qpid.Proton.Client
       /// Returns the configured default SSL port which if not set otherwise is 5671
       /// </summary>
       public int DefaultSslPort { get; set; } = DEFAULT_SSL_PORT;
+
+      /// <summary>
+      /// Controls if the default verification mechanism will allow host name mismatch
+      /// in the servers SN or Common Name field or if a mismatch fails the verification process.
+      /// By default the client uses the host used in the connection address to validate the
+      /// server name.
+      /// </summary>
+      public bool VerifyHost { get; set; } = DEFAULT_VERIFY_HOST;
+
+      /// <summary>
+      /// Configure the value used to validate the common name (server name) provided in the
+      /// servers certificate instead of using the value provided in the connection address.
+      /// This option is only used when the verify host option is enabled.
+      /// </summary>
+      public string CommonNameOverride { get; set; } = null;
+
+      /// <summary>
+      /// Controls if the client will enable the system's certificate revocation checking
+      /// feature (default is disabled).
+      /// </summary>
+      public bool EnableCertificateRevocationChecks { get; set; } = DEFAULT_ENABLED_CERT_REVOCATION_CHECKS;
+
+      /// <summary>
+      /// Allows the user to override the TLS version that the client will request from the O/S
+      /// when performing the TLS handshake.  By default the client will let the system choose the
+      /// best TLS version, however the user may wish to enforce a specifc value.
+      /// </summary>
+      public SslProtocols TlsVersionOverride { get; set; } = SslProtocols.None;
+
+      /// <summary>
+      /// Allows the user to provide an optional remote certificate validation callback which
+      /// can be used by advanced users who want to customize the validation step of the TLS
+      /// handshake process instead of relying on the built in mechanism.
+      /// </summary>
+      public RemoteCertificateValidationCallback RemoteValidationCallbackOverride { get; set; } = null;
+
+      /// <summary>
+      /// Allows the user to provide an optional local certificate selection callback which
+      /// can be used by advanced users who want to customize the selection step when choosing
+      /// the client certificate to provide to the remote during the TLS handshake.
+      /// </summary>
+      public LocalCertificateSelectionCallback LocalCertificateSelectionOverride { get; set; } = null;
 
    }
 }
