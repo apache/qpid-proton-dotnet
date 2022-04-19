db2d2a47e6f8fd7c824f46e5e22389f57040dcb4
Timothy Bish <tabish121@gmail.com>
2022-04-19 15:56:10 -0400

    Add work on packaging a release

--- build.sh
+++ build.sh
@@ -49,13 +49,60 @@ do
 
     test)
 
-       (dotnet build)
-       (dotnet test)
+       (dotnet build --configuration Release Proton.sln)
+       (LANG=en_US.UTF-8 dotnet test --configuration Release --no-build Proton.sln)
 
     ;;
 
     rat)
       mvn apache-rat:check -pl :qpid-proton-dotnet
+      ;;
+
+    dist)
+      # build source tarball for upload to apache dist staging
+      mkdir -p dist
+
+      SRC_DIR=qpid-proton-dotnet-src-$VERSION
+      BIN_DIR=qpid-proton-dotnet-bin-$VERSION
+
+      rm -rf "dist/${SRC_DIR}"
+      rm -rf "dist/${BIN_DIR}"
+
+      if [ -d .git ]; then
+        mkdir -p "dist/${SRC_DIR}"
+        mkdir -p "dist/${BIN_DIR}"
+        git archive HEAD | tar -x -C "dist/${SRC_DIR}"
+      else
+        echo "Not a GIT repo .. cannot continue"
+        exit 255
+      fi
+
+      # runs RAT on artifacts
+      mvn -N apache-rat:check
+
+      (cd dist; tar czf "../dist/${SRC_DIR}.tar.gz" "${SRC_DIR}")
+
+      # pack NuGet packages
+      dotnet pack --configuration Release Proton.sln
+
+      # Move binary artifacts to dist lib folder
+      mkdir -p "dist/${BIN_DIR}/lib"
+
+      cp -R src/Proton/bin/Release/* dist/${BIN_DIR}/lib/
+      cp -R src/Proton.Client/bin/Release/* dist/${BIN_DIR}/lib/
+
+      # add the binary LICENSE and NOTICE to the tarball
+      cp README.md LICENSE NOTICE dist/${BIN_DIR}
+
+      (cd dist; tar czf "../dist/${BIN_DIR}.tar.gz" "${BIN_DIR}")
+
+      ;;
+
+    clean)
+      rm -rf src/{Proton,Proton.Client,Proton.TestPeer}/{obj,bin}
+      rm -rf test/{Proton.Tests,Proton.Client.Tests,Proton.TestPeer.Tests}/{obj,bin}
+      rm -rf dist
+      rm -rf target
       ;;
 
     *)
